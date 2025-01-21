08af80558d8cbd617ea8c5cab6ebf60db4508538
foad <foad09332@gmail.com>
2025-01-21 08:56:18 +0200

    Features

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,23 +1,58 @@
 import { useState } from 'react'
 import '../index.css'
 
+
+
+// hidden elements
+function HandleElements({count, Decrease,condition,Increase}) {
+
+  return (
+    <>
+      <span className="count">Count: {count}</span>
+      <button type="buttom" onClick={Decrease} className="Decrease-btn" >-</button>
+       {condition && <Increase /> }
+      <h1>Let's make a recipe: </h1>
+    </>
+  )
+}
+
+// setting the + button
+function HandleIncreaseEl() {
+  return <button className="Increase-btn">+</button>
+}
+
 function Head() {
   const [ list , setList ] = useState([])
   const [ placeHolder, setPlaceHolder ] = useState('e.g. oregano')
-
-
+  const [ showHandleElements , setShowHandleElements ] = useState(false)
+  const [ count , setCount ] = useState(0)
+  const [ showHandleIncreaseEl, setShowHandleIncreaseEl ] = useState(false)
+ 
   function addNewIngredient(formData) {
     const ingredient = formData.get('ingredient')
 
-    if (ingredient !== '') {
+     if (ingredient !== '') {
       if (list.some((item => item === ingredient))) {
         setPlaceHolder(`${ingredient} is already taken`)
       } else {
         setList(prev => [...prev,  ingredient])
         setPlaceHolder('e.g. oregano')
+        setShowHandleElements(true)
+        setCount(prev => prev + 1)
       }
     } else {
       setPlaceHolder('Please enter something')
+    }
+  }
+
+  function handleDecrease() {
+    if (count > 0) {
+      setCount(count - 1)
+      // When + button is going to show
+      setShowHandleIncreaseEl(true)
+      setList((prevList) => {const newList=[...prevList]; newList.pop(); return newList;})
+    } else if (count <= 0) {
+      setShowHandleElements(false)
     }
   }
 
@@ -26,15 +61,18 @@ function Head() {
       <form action={addNewIngredient} className="head-container">
         <input 
         placeholder= {placeHolder}
-         name="ingredient" ></input>
-        <button >Add ingredient</button>
+         name="ingredient" />
+        <button>Add ingredient</button>
       </form>
 
-      <form>
+      <div>
+        <div className="pre-title" >
+          {showHandleElements && <HandleElements count={count} Decrease={handleDecrease} condition={showHandleIncreaseEl} Increase={HandleIncreaseEl}/>}
+        </div>
         <ul>
         {list.map((item) => ( <li key= {item}>{item}</li>))}
         </ul>
-      </form>
+      </div>
     </>
   )
 }
